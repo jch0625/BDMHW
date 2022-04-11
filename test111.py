f506357fae1bc88bce1ccc4a92becf7f02f25daa
jch0625 <jch0625@me.com>
2022-04-11 15:10:54 -0700

    Update test111.py

--- test111.py
+++ test111.py
@@ -1,14 +1,44 @@
 from pyspark import SparkContext
+import pandas as pd
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+
+
+def read_file(sc):
+	KFP = '/tmp/bdm/keyfood_products.csv'
+    	dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
+
+   	df1 = dffkp.withColumn('newupc',split(dffkp['upc'],'-').getItem(1))
+    	df2 = df1.withColumn("price1", regexp_extract('price', "([+-]?([0-9]*[.])?[0-9]+)" , 1 ))
+
+    	df3= df2.select('store', 'department',df2['newupc'].alias('upc'),'product','size',df2['price1'].alias('price').cast('float'))
+
+    	dfkns = pd.read_json('keyfood_nyc_stores.json')
+    	dfkns = dfkns.T
+    	dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
+
+    	dfkns2 = spark.createDataFrame(dfkns1)
+
+    	dfksi = pd.read_csv('keyfood_sample_items.csv')
+   	dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
+    	dfksi.columns = ['upc','ItemName']
+
+    	df4 = df3.join(dfksi1, df3.upc == dfksi1.upc, how = 'inner')
+
+    	df5 = df4.join(dfkns2, df4.store == dfkns2.name, how = 'inner')
+
+    	outputTask1 = df5.withColumn('food_insecurity',(df5[10]*100).cast('int')).select('ItemName','price','food_insecurity')
+    	rdd=outputTask1.rdd
+    	return rdd
+
+
 
 def main(sc):
+	df = read_file(sc)
+	sc.textFile(df.saveAsTextFile(sys.argv[2] if len(sys.argv)>2 else 'output')
 	
-   	KFP = '/tmp/bdm/keyfood_products.csv'
-    	dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
-    
-    	outputTask1 = ddffkp
-	rdd=outputTask1.rdd
 
-	sc.saveAsTextFile(rdd)
 if __name__ == "__main__":
 	sc = SparkContext()
 	main(sc)

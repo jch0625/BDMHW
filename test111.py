6155947e4bfc560719d8e766ca04312c04c4c785
jch0625 <jch0625@me.com>
2022-04-11 14:00:27 -0700

    Add files via upload

---
+++ test111.py
@@ -0,0 +1,27 @@
+from pyspark import SparkContext
+import pandas as pd
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+	sc = SparkContext()
+
+
+def main(sc):
+	KFP = '/tmp/bdm/keyfood_products.csv'
+    dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
+    dfkns = pd.read_json('keyfood_nyc_stores.json')
+    dfkns = dfkns.T
+    dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
+    dfkns2 = spark.createDataFrame(dfkns1)
+
+    dfksi = pd.read_csv('keyfood_sample_items.csv')
+    dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
+    dfksi.columns = ['upc','ItemName']
+
+    outputTask1 = ddffkp
+
+if __name__ == "__main__":
+	sc = SparkContext()
+	main(sc)

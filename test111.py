eabe70000b2c2c02d331b123c22e523cab1bc813
jch0625 <jch0625@me.com>
2022-04-11 14:22:37 -0700

    Update test111.py

--- test111.py
+++ test111.py
@@ -1,8 +1,4 @@
 from pyspark import SparkContext
-import pandas as pd
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from pyspark.sql import SparkSession
 
 if __name__ == "__main__":
 	sc = SparkContext()
@@ -12,15 +8,7 @@ def main(sc):
 	
    	KFP = '/tmp/bdm/keyfood_products.csv'
     	dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
-    	dfkns = pd.read_json('keyfood_nyc_stores.json')
-    	dfkns = dfkns.T
-    	dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
-    	dfkns2 = spark.createDataFrame(dfkns1)
-
-    	dfksi = pd.read_csv('keyfood_sample_items.csv')
-    	dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
-   	dfksi.columns = ['upc','ItemName']
-
+    
     	outputTask1 = ddffkp
 	sc.textFile(outputTask1)
 if __name__ == "__main__":

70f59747699a87b4336383f87a7934174106b792
jch0625 <jch0625@me.com>
2022-04-11 14:12:34 -0700

    Update test111.py

--- test111.py
+++ test111.py
@@ -9,19 +9,20 @@ if __name__ == "__main__":
 
 
 def main(sc):
-	KFP = '/tmp/bdm/keyfood_products.csv'
-    dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
-    dfkns = pd.read_json('keyfood_nyc_stores.json')
-    dfkns = dfkns.T
-    dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
-    dfkns2 = spark.createDataFrame(dfkns1)
+	
+   	KFP = '/tmp/bdm/keyfood_products.csv'
+    	dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
+    	dfkns = pd.read_json('keyfood_nyc_stores.json')
+    	dfkns = dfkns.T
+    	dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
+    	dfkns2 = spark.createDataFrame(dfkns1)
 
-    dfksi = pd.read_csv('keyfood_sample_items.csv')
-    dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
-    dfksi.columns = ['upc','ItemName']
+    	dfksi = pd.read_csv('keyfood_sample_items.csv')
+    	dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
+   	dfksi.columns = ['upc','ItemName']
 
-    outputTask1 = ddffkp
-
+    	outputTask1 = ddffkp
+	sc.textFile(outputTask1)
 if __name__ == "__main__":
 	sc = SparkContext()
 	main(sc)

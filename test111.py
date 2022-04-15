532b0480d844a68dbdf42e17c841b98c21aca811
jch0625 <jch0625@me.com>
2022-04-15 12:55:08 -0700

    Add files via upload

--- test111.py
+++ test111.py
@@ -1,45 +1,43 @@
-from pyspark import SparkContext
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-from pyspark.sql import SparkSession
 import csv
 import sys
 import json
 import numpy as np
 import pandas as pd
 import pyspark
+from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
-
 
 sc = pyspark.SparkContext.getOrCreate()
 spark = SparkSession(sc)
 spark
-KFP = '/tmp/bdm/keyfood_products.csv'
-dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
+keyfood_nyc_stores = spark.read.json("keyfood_nyc_stores.json")
+keyfood_nyc_stores = keyfood_nyc_stores.toPandas()
+list_name = []
+list_foodInsecurity = []
+for column in keyfood_nyc_stores:
+  list_name.append(column)
+  list_foodInsecurity.append(keyfood_nyc_stores[column][0]['foodInsecurity'])
+short_dict = dict(zip(list_name, list_foodInsecurity))
+keyfood_sample_items = spark.read.csv('keyfood_sample_items.csv', header=True, escape='"')
+keyfood_sample_items = keyfood_sample_items.withColumn("numberic_upc", F.split('UPC code', '-')[1])
+keyfood_sample_items = keyfood_sample_items.withColumnRenamed("Item Name","item_name").cache()
+keyfood_products = spark.read.csv('/tmp/bdm/keyfood_products.csv', header=True, escape='"')
+keyfood_products = keyfood_products.select('store', 'upc', 'product', 'price').dropna()
+keyfood_products = keyfood_products.withColumn("numberic_upc", F.split('upc', '-')[1]).cache()
+merge_keyfood_product = keyfood_sample_items.join(keyfood_products, (keyfood_sample_items.numberic_upc ==  keyfood_products.numberic_upc),"inner")
+merge_keyfood_product = merge_keyfood_product.select('store', 'upc', 'product', 'price').cache()
+merge_keyfood_product = merge_keyfood_product.withColumn("price", F.regexp_extract("price", '([0-9]+[.][0-9]+)', 0))
+merge_keyfood_product = merge_keyfood_product.withColumn("price",merge_keyfood_product.price.cast("float"))
+from pyspark.sql.types import StringType
+def translate(mapping):
+    def translate_(col):
+        return mapping.get(col)
+    return F.udf(translate_, StringType())
 
-df1 = dffkp.withColumn('newupc',split(dffkp['upc'],'-').getItem(1))
-df2 = df1.withColumn("price1", regexp_extract('price', "([+-]?([0-9]*[.])?[0-9]+)" , 1 ))
-
-df3= df2.select('store', 'department',df2['newupc'].alias('upc'),'product','size',df2['price1'].alias('price').cast('float'))
-
-dfkns = pd.read_json('keyfood_nyc_stores.json')
-dfkns = dfkns.T
-dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
-
-dfkns2 = spark.createDataFrame(dfkns1)
-
-dfksi = pd.read_csv('keyfood_sample_items.csv')
-dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
-dfksi.columns = ['upc','ItemName']
-
-df4 = df3.join(dfksi1, df3.upc == dfksi1.upc, how = 'inner')
-
-df5 = df4.join(dfkns2, df4.store == dfkns2.name, how = 'inner')
-
-outputTask1 = df5.withColumn('food_insecurity',(df5[10]*100).cast('int')).select('ItemName','price','food_insecurity')
-outputTask1.rdd.saveAsTextFile(sys.argv[1])
-    
-
-
-
+merge_keyfood_product = merge_keyfood_product.withColumn("foodInsecurity", translate(short_dict)("store"))
+merge_keyfood_product = merge_keyfood_product.withColumn("% Food Insecurity",100 * F.round(merge_keyfood_product.foodInsecurity.cast("float"),2))
+merge_keyfood_product = merge_keyfood_product.withColumnRenamed("product", "Item Name")
+merge_keyfood_product = merge_keyfood_product.withColumnRenamed("price", "Price ($)")
+merge_keyfood_product = merge_keyfood_product.select('Item Name', 'Price ($)', '% Food Insecurity').cache()
+merge_keyfood_product.rdd.saveAsTextFile(sys.argv[1])

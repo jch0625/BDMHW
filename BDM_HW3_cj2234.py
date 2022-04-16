5523e1071d46f00556cd38b69c767f6fb5159717
jch0625 <jch0625@me.com>
2022-04-15 17:18:52 -0700

    Add files via upload

--- BDM_HW3_cj2234.py
+++ BDM_HW3_cj2234.py
@@ -12,38 +12,35 @@ from pyspark.sql import functions as F
 from pyspark.sql import types as T
 
 
-def main(sc):
-    KFP = '/tmp/bdm/keyfood_products.csv'
-    dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
+sc = pyspark.SparkContext.getOrCreate()
+spark = SparkSession(sc)
+spark
+KFP = '/tmp/bdm/keyfood_products.csv'
+dffkp = spark.read.load(KFP, format='csv',header = True, inferSchema = True)
 
-    df1 = dffkp.withColumn('newupc',split(dffkp['upc'],'-').getItem(1))
-    df2 = df1.withColumn("price1", regexp_extract('price', "([+-]?([0-9]*[.])?[0-9]+)" , 1 ))
+df1 = dffkp.withColumn('newupc',split(dffkp['upc'],'-').getItem(1))
+df2 = df1.withColumn("price1", regexp_extract('price', "([+-]?([0-9]*[.])?[0-9]+)" , 1 ))
 
-    df3= df2.select('store', 'department',df2['newupc'].alias('upc'),'product','size',df2['price1'].alias('price').cast('float'))
+df3= df2.select('store', 'department',df2['newupc'].alias('upc'),'product','size',df2['price1'].alias('price').cast('float'))
 
-    dfkns = pd.read_json('keyfood_nyc_stores.json')
-    dfkns = dfkns.T
-    dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
+dfkns = pd.read_json('keyfood_nyc_stores.json')
+dfkns = dfkns.T
+dfkns1 = dfkns[['name','communityDistrict','foodInsecurity']]
 
-    dfkns2 = spark.createDataFrame(dfkns1)
+dfkns2 = spark.createDataFrame(dfkns1)
 
-    dfksi = pd.read_csv('keyfood_sample_items.csv')
-    dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
-    dfksi.columns = ['upc','ItemName']
+dfksi = pd.read_csv('keyfood_sample_items.csv')
+dfksi['UPC code'] = dfksi['UPC code'].apply(lambda x : x.split('-')[1])
+dfksi.columns = ['upc','ItemName']
 
-    df4 = df3.join(dfksi1, df3.upc == dfksi1.upc, how = 'inner')
+df4 = df3.join(dfksi1, df3.upc == dfksi1.upc, how = 'inner')
 
-    df5 = df4.join(dfkns2, df4.store == dfkns2.name, how = 'inner')
+df5 = df4.join(dfkns2, df4.store == dfkns2.name, how = 'inner')
 
-    outputTask1 = df5.withColumn('food_insecurity',(df5[10]*100).cast('int')).select('ItemName','price','food_insecurity')
-    outputTask1.rdd.saveAsTextFile(sys.argv[1])
+outputTask1 = df5.withColumn('food_insecurity',(df5[10]*100).cast('int')).select('ItemName','price','food_insecurity')
+outputTask1.rdd.saveAsTextFile(sys.argv[1])
     
 
 
 
-    
 
-if __name__ == "__main__":
-
-    sc = SparkContext()
-    main(sc)
